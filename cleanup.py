0d8d1c8e4b84cb2b56cbcc42d8b6494155b6a5f2
Nicole B <nicole@faenre.dev>
2021-07-22 15:08:39 -0400

    changed deletion statement

--- cleanup.py
+++ cleanup.py
@@ -14,10 +14,16 @@ BEGIN BATCH
 {0}
 APPLY BATCH;\
 """
+# DELETE_STATEMENT = """
+# DELETE FROM spans       WHERE session_id = '{0}';
+# DELETE FROM events      WHERE session_id = '{0}';
+# DELETE FROM snapshots   WHERE session_id = '{0}';
+# """
+
 DELETE_STATEMENT = """
-DELETE FROM spans       WHERE session_id = '{0}';
-DELETE FROM events      WHERE session_id = '{0}';
-DELETE FROM snapshots   WHERE session_id = '{0}';
+DELETE FROM spans       WHERE session_id IN ('{0}');
+DELETE FROM events      WHERE session_id IN ('{0}');
+DELETE FROM snapshots   WHERE session_id IN ('{0}');
 """
 
 def cleanse_spans(session):
@@ -30,7 +36,8 @@ def cleanse_spans(session):
             ids[row.session_id] = True
         counter += 1
 
-    commands = "".join([DELETE_STATEMENT.format(i) for i in ids])
+    # commands = "".join([DELETE_STATEMENT.format(i) for i in ids])
+    commands = DELETE_STATEMENT.format("', '".join(ids.keys()))
     batch = BATCH_STATEMENT.format(commands)
 
     print(batch)
