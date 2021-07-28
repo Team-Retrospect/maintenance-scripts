6c74ddc32034eca30a25dd68f79dd97e8b42240d
Nicole B <nicole@faenre.dev>
2021-07-28 10:20:39 -0400

    setup script, e.g. for docker

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+apt update
+apt install python3 python3-pip
+pip3 install cassandra-driver
+
+(crontab -l 2>/dev/null; echo "* * * * * python3 cleaner/fix_db_spans.py") | crontab -
+(crontab -l 2>/dev/null; echo "* * 0 * * python3 cleaner/cleanup.py") | crontab -
